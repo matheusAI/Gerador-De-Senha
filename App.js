2a3e61820e1efe4d4950e4aff002ce5d2c2f4bb6
matheusAI <ms2279584@gmail.com>
2023-11-28 19:28:18 -0300

    api

---
+++ App.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+
+const PORT = process.env.PORT || 3341;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+function gerarSenha(tamanhoSenha) {
+  let senha = "";
+
+  for (let i = 0; i < tamanhoSenha; i++) {
+    const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres.charAt(indiceAleatorio);
+  }
+  return senha;
+}
+
+app.use(express.json());
+
+app.post('/api/gerarSenha', (req, res) => {
+  const tamanhoSenha = req.body.tamanhoSenha || 12; // tamanho padrão de senha
+
+  const senha = gerarSenha(tamanhoSenha);
+  res.json({ senha });
+});
