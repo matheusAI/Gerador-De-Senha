2c056a92e9cf3d323bc114c77146d27d2e4367b6
matheusAI <ms2279584@gmail.com>
2023-11-29 15:40:54 -0300

    .

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 3341;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
 
 const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-function gerarSenha(tamanhoSenha) {
+app.get('/gerarSenha/:tamanhoSenha', (req, res) => {
+  const tamanhoSenha = parseInt(req.params.tamanhoSenha) || 12;
   let senha = "";
 
   for (let i = 0; i < tamanhoSenha; i++) {
     const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
     senha += caracteres.charAt(indiceAleatorio);
   }
-  return senha;
-}
 
-app.use(express.json());
-
-app.post("/api/gerarSenha", (req, res) => {
-  const tamanhoSenha = req.body.tamanhoSenha || 12; // tamanho padrão de senha
-
-  const senha = gerarSenha(tamanhoSenha);
   res.json({ senha });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`servidor rodando ${PORT}`);
 });
